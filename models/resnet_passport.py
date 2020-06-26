6b76f59d759080870b972f2279642512ed515bb6
Kam Woh <kamwoh@gmail.com>
2020-06-26 15:27:34 +0800

    fixed adaptive

--- models/resnet_passport.py
+++ models/resnet_passport.py
@@ -136,7 +136,7 @@ class ResNetPassport(nn.Module):
             out = block(out, force_passport)
         for block in self.layer4:
             out = block(out, force_passport)
-        out = F.avg_pool2d(out, 4)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
         out = out.view(out.size(0), -1)
         out = self.linear(out)
 
