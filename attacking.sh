f9b974f4b4078702fbb5aab0046424d739c7d962
Kam Woh <kamwoh@gmail.com>
2020-07-04 21:01:57 +0800

    added attack script for v1 attavk

--- attacking.sh
+++ attacking.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 GPU=\"device=${1}\" ./rundocker.sh python passport_attack_1.py --rep 10 \
-                                      --arch ${3} \
-                                      --dataset imagenet1000 \
-                                      --scheme 1 \
-                                      --loadpath ${2} \
-                                      --passport-config passport_configs/${4}_passport.json
+--arch ${3} \
+--dataset imagenet1000 \
+--scheme 1 \
+--loadpath ${2} \
+--passport-config passport_configs/${4}_passport.json
