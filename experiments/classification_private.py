cc8195b2588edf6688eb45be401a584c056cc66f
Kam Woh <kamwoh@gmail.com>
2020-08-03 15:49:54 +0800

    try to fix the problem

--- experiments/classification_private.py
+++ experiments/classification_private.py
@@ -237,7 +237,7 @@ class ClassificationPrivateExperiment(Experiment):
             # rtal = reset last layer + train all layer
             # ftal = train all layer
             try:
-                if isinstance(tl_model.classifer, nn.Sequential):
+                if isinstance(tl_model.classifier, nn.Sequential):
                     tl_model.classifier[-1].reset_parameters()
                 else:
                     tl_model.classifier.reset_parameters()
